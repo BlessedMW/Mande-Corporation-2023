6918324a6fb38a6ff7fb450730a7ff7404b7ce65
Jean Steven <82885721+BlessedMW@users.noreply.github.com>
2023-02-01 10:56:00 -0500

    Delete data_v1.sql

--- database/data_v1.sql
+++
@@ -1,9 +0,0 @@
-\c mandeCorporation
-
-INSERT INTO Cliente VALUES ('3207395712', 'Lionel Andres', 'Messi', 'm10password', 'Calle 10', 'messi@m10.com');
-
-INSERT INTO Tarjeta VALUES ('CRÉDITO', '1234567890', 'tarjetaCode', '3207395712');
-
-INSERT INTO Trabajador VALUES ('3207395713', 'Cristiano', 'Ronaldo', 'cr7password', 'Cra 7');
-
-INSERT INTO Servicio VALUES ('3207395713', 'Plomero', '$35000 por hora');
